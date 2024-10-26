99690bd4735b9b2bba37ed40b8efbd00ff4ea532
unknown <danterkum16@gmail.com>
2024-10-26 17:58:32 +0100

    Update Setup Version

--- TivWordNet/setup.py
+++ TivWordNet/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tivwordnet',
-    version='0.2.0',
+    version='0.2.1',
     description='A semantic network for the Tiv language, modeled after WordNet',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
